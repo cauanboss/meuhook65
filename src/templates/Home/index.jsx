b744be56c1e4065f6ac4996c8feab2cf50665f1c
Cauan Boss <cauanboss@gmail.com>
2022-01-09 22:24:27 -0300

    meu hook

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from 'react';
+
+const useMediaQuery = (queryValue, initialValue = false) => {
+  const [match, setMatch] = useState(initialValue);
+
+  useEffect(() => {
+    let isMounted = true;
+    const matchMedia = window.matchMedia(queryValue);
+
+    const handleChange = () => {
+      if (!isMounted) return;
+      setMatch(Boolean(matchMedia.matches));
+    };
+
+    matchMedia.addEventListener('change', handleChange);
+    setMatch(!!matchMedia.matches);
+
+    return () => {
+      isMounted = false;
+      matchMedia.removeEventListener('change', handleChange);
+    };
+  }, [queryValue]);
+
+  return match;
+};
+
 const Home = () => {
-  return (
-    <div >
-      <h1>Oi</h1>
-    </div>
-  );
-}
+  const huge = useMediaQuery('(min-width: 980px)');
+  const small = useMediaQuery('(max-width: 979px)');
+  const background = huge ? 'green' : small ? 'red' : null;
+
+  console.log(background, huge);
+
+  return <div style={{ fontSize: '60px', background }}>Oi</div>;
+};
 
 export default Home;
